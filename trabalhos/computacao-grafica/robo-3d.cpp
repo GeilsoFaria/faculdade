17dfddb851656f594ce008c7c44917eb3058dee6
GeilsoF <geilsorodriguesgf@gmail.com>
2023-08-08 10:22:09 -0400

    atualizacao desenho

--- trabalhos/computacao-grafica/robo-3d.cpp
+++ trabalhos/computacao-grafica/robo-3d.cpp
@@ -10,181 +10,202 @@ void desenharRobo() {
 
     //TRONCO
     glPushMatrix();
-    glColor3f(1.0, 0.0, 0.0);
-    glTranslatef(20.0, 20.0, 0.0);
-    glScalef(10.0, 30.0, 2.0);
-    glutSolidCube(1.0);
-    glPopMatrix();
-
+        glColor3f(1.0, 0.0, 0.0);
+        glTranslatef(20.0, 40.0, 0.0);
+        glPushMatrix();
+            glScalef(10.0, 30.0, 10.0);
+            glutSolidCube(1.0);
+        glPopMatrix();
     //Articulação pescoço
-    glPushMatrix();
-    glColor3f(0.0,1.0,0.0);
-    glTranslatef(20.0, 40.0, 0.0);
-    glutSolidSphere(5.0, 20, 20);
-    glPopMatrix();
-
+        glPushMatrix();
+            glColor3f(0.0,1.0,0.0);
+            glTranslatef(0.0, 20.0, 0.0);
+            glutSolidSphere(5.0, 20, 20);
+        glPopMatrix();
     //pescoco
-    glPushMatrix();
-    glColor3f(1.0, 0.0, 0.0);
-    glTranslatef(20.0, 48.0, 0.0); // Ajuste a posição para que o cubo rosa fique acima do cubo vermelho
-    glScalef(4.0, 5.0, 1.0);
-    glutSolidCube(1.0);
-    glPopMatrix();
+        glPushMatrix();
+            glColor3f(1.0, 0.0, 0.0);
+            glTranslatef(0, 25.0, 0.0);
+            glPushMatrix();
+                glScalef(4.0, 5.0, 10.0);
+                glutSolidCube(1.0);
+            glPopMatrix();
+        //cabeca
+            glPushMatrix();
+                glColor3f(0.0,0.0,0.0);
+                glTranslatef(0.0, 7.5, 0.0);
+                glutSolidSphere(10.0, 20, 20);
+            glPopMatrix();
+        glPopMatrix();
 
-     //cabeca
-    glPushMatrix();
-    glColor3f(0.0,0.0,0.0);
-    glTranslatef(20.0, 62.0, 0.0);
-    glutSolidSphere(10.0, 20, 20);
-    glPopMatrix();
 
     //clavicula esquerdo
     glPushMatrix();
-    glColor3f(1.0, 1.0, 0.0);
-    glTranslatef(10, 33.0, 0.0); // Ajuste a posição para que o cubo rosa fique acima do cubo vermelho
-    glScalef(30.0, 5.0, 1.0);
-    glutSolidCube(1.0);
-    glPopMatrix();
-
-    //Articulação cima braco esquerdo
-    glPushMatrix();
-    glColor3f(0.0,1.0,0.0);
-    glTranslatef(0, 25.0, 0.0);
-    glutSolidSphere(5.0, 20, 20);
-    glPopMatrix();
-
-
-   //braco esquerdo superior
-    glPushMatrix();
-    glColor3f(1.0, 1.0, 0.0);
-    glTranslatef(0, 15.0, 0.0); // Ajuste a posição para que o cubo rosa fique acima do cubo vermelho
-    glScalef(5.0, 10.0, 1.0);
-    glutSolidCube(1.0);
-    glPopMatrix();
-
-
-    //Articulação baixo braco esquerdo
-    glPushMatrix();
-    glColor3f(0.0,1.0,0.0);
-    glTranslatef(0, 10.0, 0.0);
-    glutSolidSphere(5.0, 20, 20);
-    glPopMatrix();
-
-     //braco esquerdo inferior
-    glPushMatrix();
-    glColor3f(1.0, 1.0, 0.0);
-    glTranslatef(0, 3.0, 0.0); // Ajuste a posição para que o cubo rosa fique acima do cubo vermelho
-    glScalef(5.0, 5.0, 1.0);
-    glutSolidCube(1.0);
+        glColor3f(1.0, 1.0, 0.0);
+        glTranslatef(-12.5, 12.5, 0.0);
+        glPushMatrix();
+            glScalef(15.0, 5.0, 10.0);
+            glutSolidCube(1.0);
+        glPopMatrix();
+        //Articulação cima braco esquerdo
+        glPushMatrix();
+            glColor3f(0.0,1.0,0.0);
+            glTranslatef(-12.5, -2.0, 0.0);
+            glutSolidSphere(5.0, 20, 20);
+        glPopMatrix();
+        //braco esquerdo cima
+        glPushMatrix();
+            glColor3f(1.0, 1.0, 0.0);
+            glTranslatef(-12.5, -15.0, 0.0);
+            glPushMatrix();
+            glScalef(5.0, 15.0, 10.0);
+            glutSolidCube(1.0);
+            glPopMatrix();
+        glPopMatrix();
+        //articulacao de baixo esquerda
+        glPushMatrix();
+            glColor3f(0.0,1.0,0.0);
+            glTranslatef(-12.5, -28.5, 0.0);
+            glutSolidSphere(5.0, 20, 20);
+        glPopMatrix();
+        //inferior braco esquerdo
+        glPushMatrix();
+            glColor3f(1.0, 1.0, 0.0);
+            glTranslatef(-12.5, -37.5, 0.0);
+            glScalef(7.5, 5.0, 10.0);
+            glutSolidCube(1.0);
+            glPopMatrix();
     glPopMatrix();
 
 
     //clavicula direito
     glPushMatrix();
-    glColor3f(1.0, 1.0, 0.0);
-    glTranslatef(30, 33.0, 0.0); // Ajuste a posição para que o cubo rosa fique acima do cubo vermelho
-    glScalef(30.0, 5.0, 1.0);
-    glutSolidCube(1.0);
+        glColor3f(1.0, 1.0, 0.0);
+        glTranslatef(12.5, 12.5, 0.0);
+        glPushMatrix();
+            glScalef(15.0, 5.0, 10.0);
+            glutSolidCube(1.0);
+        glPopMatrix();
+        //Articulação cima braco direito
+        glPushMatrix();
+            glColor3f(0.0,1.0,0.0);
+            glTranslatef(12.5, -2.0, 0.0);
+            glutSolidSphere(5.0, 20, 20);
+        glPopMatrix();
+        //braco direito cima
+        glPushMatrix();
+            glColor3f(1.0, 1.0, 0.0);
+            glTranslatef(12.5, -15.0, 0.0);
+            glPushMatrix();
+            glScalef(5.0, 15.0, 10.0);
+            glutSolidCube(1.0);
+            glPopMatrix();
+        glPopMatrix();
+        //articulacao de baixo direito
+        glPushMatrix();
+            glColor3f(0.0,1.0,0.0);
+            glTranslatef(12.5, -28.5, 0.0);
+            glutSolidSphere(5.0, 20, 20);
+        glPopMatrix();
+        //inferior braco direito
+        glPushMatrix();
+            glColor3f(1.0, 1.0, 0.0);
+            glTranslatef(12.5, -37.5, 0.0);
+            glScalef(7.5, 5.0, 10.0);
+            glutSolidCube(1.0);
+            glPopMatrix();
     glPopMatrix();
 
-    //Articulação cima direito
+     //cintura
     glPushMatrix();
-    glColor3f(0.0,1.0,0.0);
-    glTranslatef(40, 25.0, 0.0);
-    glutSolidSphere(5.0, 20, 20);
-    glPopMatrix();
+        glColor3f(1.0, 0.0, 0.0);
+        glTranslatef(0.0, -15.0, 0.0);
+        glPushMatrix();
+        glScalef(30.0, 5.0,10.0);
+        glutSolidCube(1.0);
+        glPopMatrix();
+        //Articulação perna esquerda superior
+        glPushMatrix();
+            glColor3f(0.0,0.0,0.0);
+            glTranslatef(-7.5, -7.5, 0.0);
+            glutSolidSphere(5.0, 20, 20);
+        glPopMatrix();
+         //perna esquerda superior
+        glPushMatrix();
+            glColor3f(1.0, 1.0, 0.0);
+            glTranslatef(-7.5, -25.0, 0.0);
+            glPushMatrix();
+            glScalef(5.0, 25.0, 10.0);
+            glutSolidCube(1.0);
+            glPopMatrix();
+        glPopMatrix();
+        //Articulação perna esquerda inferior
+        glPushMatrix();
+            glColor3f(0.0,1.0,0.0);
+            glTranslatef(-7.5, -45.0, 0.0);
+            glutSolidSphere(5.0, 20, 20);
+        glPopMatrix();
+        //perna esquerda inferior
+        glPushMatrix();
+            glColor3f(1.0, 1.0, 0.0);
+            glTranslatef(-7.5, -65.0, 0.0);
+            glPushMatrix();
+            glScalef(5.0, 25.0, 10.0);
+            glutSolidCube(1.0);
+            glPopMatrix();
+        glPopMatrix();
 
-    //braco direito superior
-    glPushMatrix();
-    glColor3f(1.0, 1.0, 0.0);
-    glTranslatef(40, 15.0, 0.0); // Ajuste a posição para que o cubo rosa fique acima do cubo vermelho
-    glScalef(5.0, 10.0, 1.0);
-    glutSolidCube(1.0);
-    glPopMatrix();
+        //--------------perna direita
+        //Articulação perna esquerda superior
+        glPushMatrix();
+            glColor3f(0.0,0.0,0.0);
+            glTranslatef(7.5, -7.5, 0.0);
+            glutSolidSphere(5.0, 20, 20);
+        glPopMatrix();
+         //perna esquerda superior
+        glPushMatrix();
+            glColor3f(1.0, 1.0, 0.0);
+            glTranslatef(7.5, -25.0, 0.0);
+            glPushMatrix();
+            glScalef(5.0, 25.0, 10.0);
+            glutSolidCube(1.0);
+            glPopMatrix();
+        glPopMatrix();
+        //Articulação perna esquerda inferior
+        glPushMatrix();
+            glColor3f(0.0,1.0,0.0);
+            glTranslatef(7.5, -45.0, 0.0);
+            glutSolidSphere(5.0, 20, 20);
+        glPopMatrix();
+        //perna esquerda inferior
+        glPushMatrix();
+            glColor3f(1.0, 1.0, 0.0);
+            glTranslatef(7.5, -65.0, 0.0);
+            glPushMatrix();
+            glScalef(5.0, 25.0, 10.0);
+            glutSolidCube(1.0);
+            glPopMatrix();
+        glPopMatrix();
 
-    //Articulação baixo braco direito
-    glPushMatrix();
-    glColor3f(0.0,1.0,0.0);
-    glTranslatef(40, 10.0, 0.0);
-    glutSolidSphere(5.0, 20, 20);
-    glPopMatrix();
+    glPopMatrix();//matriz cintura
 
-     //braco direito inferior
-    glPushMatrix();
-    glColor3f(1.0, 1.0, 0.0);
-    glTranslatef(40, 3.0, 0.0); // Ajuste a posição para que o cubo rosa fique acima do cubo vermelho
-    glScalef(5.0, 5.0, 1.0);
-    glutSolidCube(1.0);
-    glPopMatrix();
+    glPopMatrix();//matriz tronco
 
+     glColor3f(1.0,0.0,0.0);
+    glBegin(GL_LINE_LOOP);
+        glVertex3f(60.0,-28.5,30.0);
+        glVertex3f(60.0,-28.5,-30.0);
+        glVertex3f(-20.0,-28.5,-30.0);
+        glVertex3f(-20.0,-28.5,30.0);
+    glEnd();
 
-    //cintura
-    glPushMatrix();
-    glColor3f(1.0, 0.0, 0.0);
-    glTranslatef(20.0, 5.0, 0.0);
-    glScalef(30.0, 5.0,1.0);
-    glutSolidCube(1.0);
-    glPopMatrix();
-
-    //Articulação perna esquerda superior
-    glPushMatrix();
-    glColor3f(0.0,1.0,0.0);
-    glTranslatef(10.0, -2.5, 0.0);
-    glutSolidSphere(5.0, 20, 20);
-    glPopMatrix();
-
-    //perna esquerda superior
-    glPushMatrix();
-    glColor3f(1.0, 1.0, 0.0);
-    glTranslatef(10, -15.0, 0.0); // Ajuste a posição para que o cubo rosa fique acima do cubo vermelho
-    glScalef(5.0, 25.0, 1.0);
-    glutSolidCube(1.0);
-    glPopMatrix();
-
-    //Articulação perna esquerda inferior
-    glPushMatrix();
-    glColor3f(0.0,1.0,0.0);
-    glTranslatef(10.0, -25.0, 0.0);
-    glutSolidSphere(5.0, 20, 20);
-    glPopMatrix();
-
-    //perna esquerda inferior
-    glPushMatrix();
-    glColor3f(1.0, 1.0, 0.0);
-    glTranslatef(10, -30.0, 0.0); // Ajuste a posição para que o cubo rosa fique acima do cubo vermelho
-    glScalef(5.0, 25.0, 1.0);
-    glutSolidCube(1.0);
-    glPopMatrix();
-
-    //Articulação perna direita superior
-    glPushMatrix();
-    glColor3f(0.0,1.0,0.0);
-    glTranslatef(31.0, -2.5, 0.0);
-    glutSolidSphere(5.0, 20, 20);
-    glPopMatrix();
-
-     //perna direita superior
-    glPushMatrix();
-    glColor3f(1.0, 1.0, 0.0);
-    glTranslatef(31, -15.0, 0.0); // Ajuste a posição para que o cubo rosa fique acima do cubo vermelho
-    glScalef(5.0, 25.0, 1.0);
-    glutSolidCube(1.0);
-    glPopMatrix();
-
-    //Articulação perna direita inferior
-    glPushMatrix();
-    glColor3f(0.0,1.0,0.0);
-    glTranslatef(31.0, -25.0, 0.0);
-    glutSolidSphere(5.0, 20, 20);
-    glPopMatrix();
-
-    //perna esquerda inferior
-    glPushMatrix();
-    glColor3f(1.0, 1.0, 0.0);
-    glTranslatef(31.0, -30.0, 0.0); // Ajuste a posição para que o cubo rosa fique acima do cubo vermelho
-    glScalef(5.0, 25.0, 1.0);
-    glutSolidCube(1.0);
-    glPopMatrix();
+    /* glColor3f(1.0,0.0,0.0);
+    glBegin(GL_LINE_LOOP);
+        glVertex3f(60.0,0.0,30.0);
+        glVertex3f(60.0,0.0,-30.0);
+        glVertex3f(-20.0,0.0,-30.0);
+        glVertex3f(-20.0,0.0,30.0);
+    glEnd();*/
 
     glutSwapBuffers();
 }
@@ -196,8 +217,8 @@ void resize(int w, int h) {
     gluPerspective(120.0, (GLdouble)w / h, 1.0, 100.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(-2.80, 10.0, 60.0,
-              0.0, 0.0, 0.0,
+    gluLookAt(0.00, 40.0, 60.0,
+              20.0, 20.0, 0.0,
               0.0, 1.0, 0.0);
 }
 
