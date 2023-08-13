e80a49c7191913372eb2276d804d3f1095f30da1
GeilsoF <geilsorodriguesgf@gmail.com>
2023-08-13 14:42:21 -0400

    caminhada manual

--- trabalhos/computacao-grafica/robo2-3d.cpp
+++ trabalhos/computacao-grafica/robo2-3d.cpp
@@ -47,23 +47,28 @@ bool controleBraco=true;
 bool caminhando = false;
 float anguloMovCabecaPescocoY = 0.0f;
 float anguloMovCabecaPescocoX = 0.0f;
-float anguloPernaEsquerda = 0.0f;
-float anguloPernaDireita = 0.0f;
+float anguloPernaSuperior = 0.0f;
+float anguloPernaInferior = 0.0f;
+float anguloPernaSuperiorDireita = 0.0f;
+float anguloPernaInferiorDireita = 0.0f;
 float anguloBracoX =0.0f;
 float anguloBracoY=0.0f;
 float anguloBracoZ=0.0f;
-float posicaoRoboX = 0.0; // Posição do robô no eixo X
+float posicaoAnimacaoZ = 0.0; // Posição do robô no eixo X
 int passo = 0; // Contador de passos
-
-
-
+float MovimentoPeZ=0.0;
+float MovimentoPeDireitaZ=0.0;
 //--------Declaração de Funções--------
 void GirarBraco(int value);
 void GirarCabeca(int value);
 void GirarPescoco(int value);
 void desenharArticulacao();
 
+
+int valor=30;
+
 //-------------MOVIMENTACAO--------------
+
 void GirarBraco(int value)
 {
     if(controleBraco==true)
@@ -97,7 +102,9 @@ void GirarBraco(int value)
     }
     glutPostRedisplay();
     glutTimerFunc(100, GirarBraco, 0);
+
 }
+
 void GirarCabeca(int value)
 {
     if (girandoCabecaEsquerda)
@@ -147,44 +154,45 @@ void GirarPescoco(int value)
     glutTimerFunc(33, GirarPescoco, 0);
 }
 //-------------------$ CAMINHADA $ -------------------
-void AtualizarAnimacao(int value) {
-    // Atualiza o ângulo das pernas de acordo com o passo atual
-    if (caminhando) {
-        if (passo % 2 == 0) {
-            anguloPernaEsquerda = sin(value * 0.1) * 45.0;  // Simula o movimento da perna esquerda
-            anguloPernaDireita = sin(value * 0.1 + 3.14) * 45.0;  // Mantém a perna direita reta
-        } else {
-            anguloPernaEsquerda = sin(value * 0.1 + 3.14) * 45.0;  // Mantém a perna esquerda reta
-            anguloPernaDireita = sin(value * 0.1) * 45.0;  // Simula o movimento da perna direita
-        }
+void caminhadaManual(int valor)
+{
+    anguloPernaSuperior = sin(valor * 0.1) * 3.5;  // Simula o movimento da perna esquerda
+    anguloPernaInferior = sin(valor * 0.1 + 3.14) * 40.0;  // Mantém a perna direita reta
+    float t = 0.2 + 0.3 * sin(valor * 0.03);
+    //MovimentoPeZ = t;
+    anguloPernaSuperiorDireita = cos(valor * 0.1) * 3.5;  // Simula o movimento da perna esquerda
+    anguloPernaInferiorDireita = cos(valor * 0.1 + 3.14) * 40.0;  // Mantém a perna direita reta
+    float te = 0.2 + 0.3 * cos(valor * 0.03);
+   // MovimentoPeDireitaZ += te;
 
-        // Move o robô para frente durante a caminhada
-        if (passo < 2) {
-            posicaoRoboX += 0.05; // Ajuste a velocidade de movimento conforme necessário
-        } else {
-            caminhando = false; // Termina a animação após dois passos
-        }
+    posicaoAnimacaoZ += 5.0; // Ajuste a velocidade de movimento conforme necessário
+    glutPostRedisplay();
+}
+void AtualizarAnimacao(int value)
+{
+    // Atualiza o ângulo das pernas de acordo com o passo atual
+    if (caminhando)
+    {
+            anguloPernaSuperior = sin(value * 0.1) * 0.5;  // Simula o movimento da perna esquerda
+            anguloPernaInferior = sin(value * 0.1 + 3.14) * 10.0;  // Mantém a perna direita reta
+            float t = 0.2 + 0.3 * sin(value * 0.03);
+            MovimentoPeZ = t;
+            posicaoAnimacaoZ += 0.05; // Ajuste a velocidade de movimento conforme necessário
     }
     glutPostRedisplay();
-    glutTimerFunc(33, AtualizarAnimacao, value + 1);
+    glutTimerFunc(15, AtualizarAnimacao, value + 1);
 }
-// Função para iniciar a animação de caminhada
-void IniciarCaminhada() {
-    caminhando = true;
-    passo = 0;
-}
-// Função para parar a animação de caminhada
-void PararCaminhada() {
-    caminhando = false;
-}
-void ConcluirPasso(int value)
+void AtualizarAnimacaoDireita(int value)
 {
-    passo++;
-    if (passo < 2) {
-        glutTimerFunc(1000, ConcluirPasso, 0);  // Espera 1 segundo antes de dar o próximo passo
-    } else {
-        PararCaminhada(); // Para a animação após dois passos
+    if (caminhando)
+    {
+        anguloPernaSuperiorDireita = cos(value * 0.1) * 0.5;  // Simula o movimento da perna esquerda
+        anguloPernaInferiorDireita = cos(value * 0.1 + 3.14) * 10.0;  // Mantém a perna direita reta
+        float t = 0.2 + 0.3 * cos(value * 0.03);
+        MovimentoPeDireitaZ = t;
     }
+    glutPostRedisplay();
+    glutTimerFunc(15, AtualizarAnimacaoDireita, value + 1);
 }
 //-------------------^MOVIMENTACAO^-------------------
 void desenharCintura()
@@ -200,32 +208,52 @@ void desenharPernaSuperior()
     glColor3f(1.0, 0.0, 1.0);
     glPushMatrix();
         glScalef(pernaSuperiorX,pernaSuperiorY,tamanhoZ);
-         glRotatef(anguloPernaEsquerda, 1.0, 0.0, 0.0);
+        glTranslatef(0.0,0.0,MovimentoPeZ);
+         glRotatef(anguloPernaSuperior, 1.0, 0.0, 0.0);
         glutSolidCube(1.0);
     glPopMatrix();
 
 }
-
 void desenharPernaInferior()
 {
     glColor3f(1.0, 0.0, 1.0);
     glPushMatrix();
         glScalef(pernaInferiorX,pernaInferiorY, tamanhoZ);
-        glRotatef(anguloPernaDireita, 1.0, 0.0, 0.0);  // Aplica a rotação da perna
+        //glTranslatef(0.0, 0.0, posicaoRoboZ);
+        glRotatef(anguloPernaInferior, 1.0, 0.0, 0.0);  // Aplica a rotação da perna
+        glTranslatef(0.0,0.0,MovimentoPeZ);
+        glutSolidCube(1.0);
+    glPopMatrix();
+}
+void desenharPernaSuperiorDireita()
+{
+    glColor3f(0.5, 0.5, 1.0);
+    glPushMatrix();
+        glScalef(pernaSuperiorX,pernaSuperiorY,tamanhoZ);
+         glRotatef( anguloPernaSuperiorDireita , 1.0, 0.0, 0.0);
+         glTranslatef(0.0,0.0,MovimentoPeDireitaZ);
         glutSolidCube(1.0);
     glPopMatrix();
 
+}
+void desenharPernaInferiorDireita()
+{
+    glColor3f(0.5, 0.5, 1.0);
+    glPushMatrix();
+        glScalef(pernaInferiorX,pernaInferiorY, tamanhoZ);
+        //glTranslatef(0.0, 0.0, posicaoRoboZ);
+        glRotatef(anguloPernaInferiorDireita, 1.0, 0.0, 0.0);  // Aplica a rotação da perna
+        glTranslatef(0.0,0.0,MovimentoPeDireitaZ);
+        glutSolidCube(1.0);
+    glPopMatrix();
 }
 void desenharInferiores()
 {
     //----INFERIORES----
     //Perna Esquerda
-    //glTranslatef(0.0,-troncoY,0.0);
-
     glTranslatef(0.0,(bracoInferiorY/2+raioEsfera*2+bracoSuperiorY+raioEsfera),0.0);//Volta o eixo para o centro de massa da esfera ao lado da clavívula
     glTranslatef(-(bracoInferiorX+claviculaX+troncoX/2),-(troncoY+cinturaY),0.0);//Posiciona para desenhar a cintura
     glTranslatef(-cinturaX/2,0.0,0.0);//Aqui arrasta o eixo do meio exato da cintura para a metade esquerda
-
     desenharCintura();
     glTranslatef(-(cinturaX/2-raioEsfera),-(cinturaY+raioEsfera/2),0.0);
     desenharArticulacao();
@@ -242,13 +270,11 @@ void desenharInferiores()
     glTranslatef((cinturaX/2-raioEsfera),-(cinturaY+raioEsfera/2),0.0);
     desenharArticulacao();
     glTranslatef(0.0,-(raioEsfera+pernaSuperiorY/2),0.0);
-    desenharPernaSuperior();
+    desenharPernaSuperiorDireita();
     glTranslatef(0.0,-(raioEsfera+pernaSuperiorY/2),0.0);
     desenharArticulacao();
     glTranslatef(0.0,-(raioEsfera+pernaInferiorY/2),0.0);
-    desenharPernaInferior();
-
-
+    desenharPernaInferiorDireita();
 }
 void desenharTronco()
 {
@@ -264,7 +290,6 @@ void desenharPescoco()
     glPushMatrix();
         glRotatef(anguloMovCabecaPescocoY,0.0,1.0,0.0);
         glRotatef(anguloMovCabecaPescocoX,1.0,0.0,0.0);
-        //glRotatef(anguloPescoco,1.0,0.0,0.0);
         glScalef(pescocoX, pescocoY, tamanhoZ);
         glutSolidCube(1.0);
     glPopMatrix();
@@ -277,21 +302,19 @@ void desenharArticulacaoSuperiores()
         glTranslatef(anguloBracoX,anguloBracoY,anguloBracoZ);
         glutSolidSphere(raioEsfera, 20, 20);
     glPopMatrix();
-
 }
 void desenharArticulacao()
 {
     glPushMatrix();
         glColor3f(0.0,1.0,0.0);
+        glTranslatef(0.0,0.0,MovimentoPeDireitaZ);
         glutSolidSphere(raioEsfera, 20, 20);
     glPopMatrix();
-
 }
 void desenharClavicula()
 {
     glColor3f(0.0, 0.0, 1.0);
     glPushMatrix();
-        //glTranslatef(anguloBracoLateral,anguloBracoY,angulobracoZ);
         glScalef(claviculaX, claviculaY, tamanhoZ);
         glutSolidCube(1.0);
     glPopMatrix();
@@ -300,11 +323,10 @@ void desenharBracoSuperior()
 {
     glColor3f(0.0, 1.0, 1.0);
     glPushMatrix();
-       // glTranslatef(0.0,anguloBracoY,angulobracoZ);
        glTranslatef(anguloBracoX,anguloBracoY,anguloBracoZ);
         glScalef(bracoSuperiorX, bracoSuperiorY, tamanhoZ);
-       // glRotatef(anguloPernaDireita, 1.0, 0.0, 0.0);  // Aplica a rotação da perna ao braco
-
+        glRotatef(anguloPernaSuperior, 1.0, 0.0, 0.0);
+        //glTranslatef(0.0,0.0,MovimentoPeDireitaZ);
         glutSolidCube(1.0);
     glPopMatrix();
 }
@@ -314,7 +336,8 @@ void desenharBracoInferior()
     glPushMatrix();
         glTranslatef(anguloBracoX,anguloBracoY,anguloBracoZ);
         glScalef(bracoInferiorX, bracoInferiorY, tamanhoZ);
-        //glRotatef(anguloPernaEsquerda, 1.0, 0.0, 0.0);
+         glRotatef(anguloPernaInferior, 1.0, 0.0, 0.0);  // Aplica a rotação da perna
+         //glTranslatef(0.0,0.0,MovimentoPeDireitaZ);
         glutSolidCube(1.0);
     glPopMatrix();
 }
@@ -343,7 +366,6 @@ void desenharSuperiores()
     //---CABECA---
     glTranslatef(0.0,(pescocoY/2+cabecaY/2),0.0);
     desenharCabeca();
-
     //---------BRACO ESQUERDO---------
     //---Clavícula Esquerda---
     glTranslatef( -(claviculaX/2+troncoX/2),-((raioEsfera*2+pescocoY+raioEsfera)+claviculaY/2) ,0.0);
@@ -353,49 +375,125 @@ void desenharSuperiores()
     desenharArticulacao();
     //Braco Superior Esquerdo
     glTranslatef(0.0,-(raioEsfera+bracoSuperiorY/2),0.0);
-    //glPushMatrix();
-   // glTranslatef(anguloBracoLateral,anguloBraco,angulobracoZ);
     desenharBracoSuperior();
-    //glPopMatrix();
     glTranslatef(0.0,-(raioEsfera+bracoSuperiorY/2),0.0);
-    //glTranslatef(anguloBracoLateral,anguloBraco,angulobracoZ);
     desenharArticulacaoSuperiores();
     //Braco Inferior Esquerdo
     glTranslatef(0.0,-(raioEsfera+bracoInferiorY/2),0.0);
-    //glTranslatef(anguloBracoLateral,anguloBraco,angulobracoZ);
     desenharBracoInferior();
-    //glPopMatrix();
-    //glTranslatef(0.0,-bracoSuperiorY*2,0);
-
-
-
     //---BRACO DIREITO ---
     glTranslatef((bracoInferiorX+claviculaX+troncoX+claviculaX/2),(bracoInferiorY+raioEsfera*2+bracoSuperiorX+claviculaY),0.0);
     //Clavicula Direita
     desenharClavicula();
     glTranslatef((claviculaX/2+raioEsfera),0.0,0.0);
     desenharArticulacao();
-   // glPushMatrix();
-    //glTranslatef(0.0,anguloBraco,angulobracoZ);
     glTranslatef(0.0,-(raioEsfera+bracoSuperiorY/2),0.0);
     desenharBracoSuperior();
     glTranslatef(0.0,-(raioEsfera+bracoSuperiorY/2),0.0);
-    //glTranslatef(0.0,anguloBraco,angulobracoZ);
     desenharArticulacaoSuperiores();
     glTranslatef(0.0,-(raioEsfera+bracoInferiorY/2),0.0);
-     //glTranslatef(0.0,anguloBraco,angulobracoZ);
     desenharBracoInferior();
-    //glPopMatrix();
 }
 void desenharRobo()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
-        glTranslatef(0.0, 0.0, posicaoRoboX);
+        glTranslatef(0.0, 0.0, posicaoAnimacaoZ);
         desenharSuperiores();
         desenharInferiores();
-        glutSwapBuffers();
     glPopMatrix();
+
+    glColor3f(0.0,0.0,0.0);
+    glBegin(GL_LINE_LOOP);
+        glVertex3f(40.0,-66.5,40.0);
+        glVertex3f(40.0,-66.5,-40.0);
+        glVertex3f(-40.0,-66.5,-40.0);
+        glVertex3f(-40.0,-66.5,40.0);
+    glEnd();
+    glutSwapBuffers();
+}
+void MenuGestos(int op)
+{
+    switch(op)
+    {
+        case 0:
+            controleBraco=true;
+            glutTimerFunc(33, GirarBraco, 0);
+            break;
+        case 1:
+            if (!girandoCabecaEsquerda && !girandoCabecaDireita)
+            {
+                girandoCabecaEsquerda = true;
+                glutTimerFunc(33, GirarCabeca, 0);
+            }
+            break;
+        case 2:
+            if (!girandoPescocoTras && !girandoPescocoFrente)
+            {
+                girandoPescocoTras = true;
+                glutTimerFunc(33, GirarPescoco, 0);
+            }
+            break;
+    }
+
+    glutPostRedisplay();
+}
+
+// Gerenciamento do menu com as op��es de cores
+void MenuCaminhada(int op)
+{
+   switch(op)
+   {
+        case 0:
+            caminhando=false;
+            passo = 0;
+            //posicaoAnimacaoZ = 0.0;
+            caminhadaManual(valor);
+            valor+=10;
+            break;
+        case 1:
+            caminhando = true;
+            passo = 0;
+            posicaoAnimacaoZ = 0.0;
+            break;
+
+    }
+    glutPostRedisplay();
+}
+
+// Gerenciamento do menu principal
+void MenuPrincipal(int op)
+{
+}
+
+// Criacao do Menu
+void CriaMenu()
+{
+    int menu,submenu1,submenu2;
+
+	submenu1 = glutCreateMenu(MenuCaminhada);
+	glutAddMenuEntry("Manualmente",0);
+	glutAddMenuEntry("Automatico",1);
+
+
+    submenu2 = glutCreateMenu(MenuGestos);
+	glutAddMenuEntry("Acenar",0);
+	glutAddMenuEntry("Mover Cabeca",1);
+	glutAddMenuEntry("Move Pescoco",2);
+
+    menu = glutCreateMenu(MenuPrincipal);
+	glutAddSubMenu("Gestos",submenu2);
+    glutAddSubMenu("Caminhar",submenu1);
+
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
+void GerenciaMouse(int button, int state, int x, int y)
+{
+    if (button == GLUT_RIGHT_BUTTON)
+         if (state == GLUT_DOWN)
+            CriaMenu();
+
+    glutPostRedisplay();
 }
 void Teclado(unsigned char tecla, int x, int y)
 {
@@ -422,9 +520,7 @@ void Teclado(unsigned char tecla, int x, int y)
             }
             break;
         case 'a'://Acenar - a
-            //anguloBracoX=0.0;
-            //anguloBracoY=0.0;
-            //anguloBracoZ=0.0;
+
             //desenharBracoInferior();
             //desenharBracoSuperior();
             controleBraco=true;
@@ -433,11 +529,19 @@ void Teclado(unsigned char tecla, int x, int y)
         case 'w': // Inicia Para a caminhada
             caminhando = true;
             passo = 0;
-            posicaoRoboX = 0.0;
+            posicaoAnimacaoZ = 0.0;
             break;
         case 'p':
             caminhando=false;
             break;
+         case 'r'://Acenar - a
+            anguloBracoX=0.0;
+            anguloBracoY=0.0;
+            anguloBracoZ=0.0;
+            break;
+         case 'm':
+            caminhadaManual(valor);
+            valor+=10;
     }
 }
 void resize(int w, int h)
@@ -448,7 +552,7 @@ void resize(int w, int h)
     gluPerspective(120.0, (GLdouble)w / h, 1.0, 100.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(-50.0, 0.0, 60.0,
+    gluLookAt(-40.0, 0.0, 60.0,
               0.0, 0.0, 0.0,
               0.0, 1.0, 0.0);
 }
@@ -456,7 +560,7 @@ int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(500, 500);
+    glutInitWindowSize(600, 600);
     glutCreateWindow("Robô 3D com 9 articulações");
     glutReshapeFunc(resize);
     glClearColor(1.0, 1.0, 1.0, 1.0);
@@ -464,6 +568,8 @@ int main(int argc, char** argv)
     glutDisplayFunc(desenharRobo);
     glutKeyboardFunc (Teclado);
     glutTimerFunc(33, AtualizarAnimacao, 0);
+    glutTimerFunc(33, AtualizarAnimacaoDireita, 0);
+    glutMouseFunc(GerenciaMouse);
 
     glutMainLoop();
     return 0;
